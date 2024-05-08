59e4b8ac6bb5d5d9b9ef48e45fee91ea45a2b70a
Pranav K <pranavkcse@gmail.com>
2024-05-08 18:11:59 +0530

    array to string

--- Strings/problems.js
+++ Strings/problems.js
@@ -9,4 +9,18 @@ for (let i = 0; i < str.length; i++) {
     list.prepend(str[i])
 }
 
+
+// Array to List
+let arr = [1,23,4,5,66,78,98,10]
+
+let array = new DoublyLinkedList()
+
+for (let i = 0; i < arr.length; i++) {
+    
+    array.append(arr[i])
+    
+}
+
+array.display()
+
 list.display()
