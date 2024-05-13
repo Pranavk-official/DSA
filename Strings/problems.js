9877e3302a958f3495f0c40be416b471ecedf73c
Pranav K <pranavkcse@gmail.com>
2024-05-13 12:19:03 +0530

    string problems

--- Strings/problems.js
+++ Strings/problems.js
@@ -1,26 +1,32 @@
 const DoublyLinkedList = require('../Linked Lists/Implementations/double')
 
 
-let str = 'Hello World'
+let str = 'Hi Hrithunath'
 
 let list = new DoublyLinkedList()
 
 for (let i = 0; i < str.length; i++) {
     list.prepend(str[i])
 }
-
-
-// Array to List
-let arr = [1,23,4,5,66,78,98,10]
-
-let array = new DoublyLinkedList()
-
-for (let i = 0; i < arr.length; i++) {
-    
-    array.append(arr[i])
-    
+let revString = '';
+let currentNode = list.head
+for (let i = 0; i < list.size ; i++, currentNode = currentNode.next) {
+    revString += currentNode.data
 }
 
-array.display()
+console.log(revString);
+
+// Array to List
+// let arr = [1,23,4,5,66,78,98,10]
+
+// let array = new DoublyLinkedList()
+
+// for (let i = 0; i < arr.length; i++) {
+    
+//     array.append(arr[i])
+    
+// }
+
+// array.display()
 
 list.display()
